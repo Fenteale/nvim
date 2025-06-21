550795c822fc3238f63486d81b806c334a2e601a
Mangmasta <mangmasta@gmail.com>
2025-06-20 23:45:33 -0700

    Simple nvim setup with telescope and treesitter

--- init.lua
+++ init.lua
@@ -1,2 +1,15 @@
--- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+vim.g.mapleader = " "
+
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<C-p>', builtin.find_files, { desc = 'Telescope find files' })
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
+vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
+
+require'nvim-treesitter.configs'.setup {
+	ensure_installed = { "c", "lua", "python" },
+	highlight = { enable = true },
+	indent = { enable = true },
+}
